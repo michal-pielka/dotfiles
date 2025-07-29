1c693f52a2e2f92899cc7f10e4d669ac4790bb33
Michal Pielka <mpielka726@gmail.com>
2025-07-29 18:29:06 -0400

    feat: add auto formatting on file save

--- nvim/lua/configs/conform.lua
+++ nvim/lua/configs/conform.lua
@@ -3,16 +3,16 @@ local options = {
     lua = { "stylua" },
     css = { "prettier" },
     html = { "prettier" },
-    python = { "black" },
-    rust = { "rustfmt" }, -- Add this line
-    go = { "goimports" },  -- Add this line
+    python = { "ruff" },
+    rust = { "rustfmt" },
+    go = { "goimports" },
   },
 
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
 }
 
 return options
