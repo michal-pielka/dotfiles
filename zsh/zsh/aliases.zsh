45b3e45a6fc9f43fb5fc8ea7222becfce20bfe69
Michal Pielka <mpielka726@gmail.com>
2025-08-07 21:27:14 -0400

    feat: add ng: open file with nvim using fzf with live grep

--- zsh/zsh/aliases.zsh
+++ zsh/zsh/aliases.zsh
@@ -11,26 +11,6 @@ alias n='nvim'
 
 alias ff='fastfetch'
 
-# fd, fzf aliases
-# Basically f='fd | fzf' with option to use flags like -e py, -t f etc.
-f() {
-  fd "$@" | fzf
-}
-
-# nvim with fzf
-# The following is basically: nf='nvim $(fd | fzf --multi' with the option to abort and add flags like -e py, -t f etc.
-# Default behavior is to only search for files (-t f).
-nf() {
-  # Run fd with a default type of 'file', but allow user to add/override with their own flags ("$@").
-  # For example, `nf -t d` will work because the user's -t d flag overrides the default -t f.
-  files=($(fd --type file "$@" | fzf --multi))
-
-  # If we got at least one selection, open with nvim.
-  if [ ${#files[@]} -gt 0 ]; then
-    nvim "${files[@]}"
-  fi
-}
-
 # eza / ls
 alias ls='LS_COLORS= eza --git --color=always --group-directories-first --header --sort=name'
 alias lt='ls --tree'
--- zsh/zsh/functions.zsh
+++ zsh/zsh/functions.zsh
@@ -10,3 +10,44 @@ cdf() {
   fi
 }
 
+# fd, fzf aliases
+# Basically f='fd | fzf' with option to use flags like -e py, -t f etc.
+f() {
+  fd "$@" | fzf
+}
+
+# nvim with fzf
+# The following is basically: nf='nvim $(fd | fzf --multi' with the option to abort and add flags like -e py, -t f etc.
+# Default behavior is to only search for files (-t f).
+nf() {
+  # Run fd with a default type of 'file', but allow user to add/override with their own flags ("$@").
+  # For example, `nf -t d` will work because the user's -t d flag overrides the default -t f.
+  files=($(fd --type file "$@" | fzf --multi))
+
+  # If we got at least one selection, open with nvim.
+  if [ ${#files[@]} -gt 0 ]; then
+    nvim "${files[@]}"
+  fi
+}
+
+# nvim with ripgrep and fzf (live grep)
+# The idea is to use fzf to select a file from the output of ripgrep,
+# and then open it in nvim.
+ng() {
+  # Use ripgrep to search for a pattern in all files.
+  # The output is piped to fzf for interactive selection.
+  # We use --preview with bat to show the file content with syntax highlighting.
+  # The selected line format is "file:line_number:...", so we use awk to extract the file path.
+  local file
+  file=$(rg --line-number --no-heading --color=always "${1-}" | \
+    fzf --ansi \
+        --color "hl:-1:underline,hl+:-1:underline:reverse" \
+        --delimiter ":" \
+        --preview 'bat --style=full --color=always --highlight-line {2} {1}' | \
+    awk -F: '{print $1}')
+
+  # If a file was selected, open it in nvim.
+  if [[ -n "$file" ]]; then
+    nvim "$file"
+  fi
+}
