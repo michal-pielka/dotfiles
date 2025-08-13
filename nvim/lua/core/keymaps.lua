543ebf9188eef64a24d1038cb0c3bee378e31ce9
Michal Pielka <mpielka726@gmail.com>
2025-08-12 21:16:37 -0400

    feat: add keymaps to change splits easily

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -1,8 +1,6 @@
-vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>')
-
 -- Writing and quitting
-vim.keymap.set('n', '<leader>w', ':write<CR>')
-vim.keymap.set('n', '<leader>q', ':quit<CR>')
+vim.keymap.set('n', '<leader>w', '<CMD>write<CR>')
+vim.keymap.set('n', '<leader>q', '<CMD>quit<CR>')
 
 -- Clear highlights on search when pressing <Esc> in normal mode
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
@@ -13,3 +11,10 @@ vim.keymap.set('i', 'jk', '<Esc>')
 -- Center screen when going half a page up/down
 vim.keymap.set('n', '<C-d>', '<C-d>zz')
 vim.keymap.set('n', '<C-u>', '<C-u>zz')
+
+
+-- Make split navigation easier.
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
