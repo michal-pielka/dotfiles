577794d463193953fbc49474440f3d7e174dd51c
Michal Pielka <mpielka726@gmail.com>
2025-08-26 00:12:17 +0200

    feat: add some buffer keymaps

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -18,3 +18,10 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- Buffer navigation
+vim.keymap.set("n", "<C-,>", ":bprevious<CR>", { desc = "Previous buffer" })
+vim.keymap.set("n", "<C-.>", ":bnext<CR>", { desc = "Next buffer" })
+
+-- Close buffer
+vim.keymap.set("n", "<C-q>", ":bdelete<CR>", { desc = "Delete buffer" })
