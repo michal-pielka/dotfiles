8cf9b2393f9d31030bc5da257a95c3d875365809
Michal Pielka <mpielka726@gmail.com>
2025-09-16 22:45:59 +0200

    chore: remove ts objects for now

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -7,25 +7,5 @@ return {
     highlight = {
       enable = true,
     },
-    -- language-aware textobjects: prefer these for af/if/ac/ic, etc.
-    textobjects = {
-      select = {
-        enable = true,
-        lookahead = true, -- jump forward to textobj
-        keymaps = {
-          -- outer/inner mappings commonly used
-          ['ac'] = '@class.outer',
-          ['ic'] = '@class.inner',
-          ['af'] = '@function.outer',
-          ['if'] = '@function.inner',
-		  ['al'] = '@loop.outer',
-		  ['il'] = '@loop.inner',
-		  ['ai'] = '@conditional.outer',
-		  ['ii'] = '@conditional.inner',
-        },
-      },
-      -- optionally configure move/swap here if you want:
-      -- move = { ... }, swap = { ... }
-    },
   },
 }
