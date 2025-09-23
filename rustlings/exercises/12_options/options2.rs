a8d5bb393621a1daec00a71769be7f3f061e4cf3
Michal Pielka <mpielka726@gmail.com>
2025-09-23 23:23:34 +0200

    feat: add some junk i want to keep - fucked up my asahi setup, needs reinstallingXD

---
+++ rustlings/exercises/12_options/options2.rs
@@ -0,0 +1,37 @@
+fn main() {
+    // You can optionally experiment here.
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn simple_option() {
+        let target = "rustlings";
+        let optional_target = Some(target);
+
+        // TODO: Make this an if-let statement whose value is `Some`.
+        if let Some(word) = optional_target {
+            assert_eq!(word, target);
+        }
+    }
+
+    #[test]
+    fn layered_option() {
+        let range = 10;
+        let mut optional_integers: Vec<Option<i8>> = vec![None];
+
+        for i in 1..=range {
+            optional_integers.push(Some(i));
+        }
+
+        let mut cursor = range;
+
+        while let Some(some_integer) = optional_integers.pop() {
+            if let Some(_integer) = some_integer {
+                cursor -= 1;
+            }
+        }
+
+        assert_eq!(cursor, 0);
+    }
+}
