92ef04876220642a79f530104d7929642219d2c1
Michal Pielka <mpielka726@gmail.com>
2025-10-30 17:06:32 +0100

    fix: temporary fix for clangd LSP on asahi

--- nvim/lua/core/autocmds.lua
+++ nvim/lua/core/autocmds.lua
@@ -5,3 +5,18 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 		vim.hl.on_yank()
 	end,
 })
+
+-- The following is only meant to be used on asahi linux
+-- we use native clangd since mason fails to install it
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "c", "cpp" },
+  callback = function()
+    vim.lsp.start {
+      name = "clangd",
+      cmd = { "/usr/bin/clangd" },
+      root_dir = vim.fs.dirname(
+        vim.fs.find({ ".git", "compile_commands.json", "compile_flags.txt" }, { upward = true })[1]
+      ),
+    }
+  end,
+})
