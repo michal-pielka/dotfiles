4771e1dac4c1fa7ea135c1413f1360b3ba773c7e
Michal Pielka <mpielka726@gmail.com>
2025-11-15 04:39:11 +0100

    feat: add blueprint

--- dotfiles_setup/setup.sh
+++ dotfiles_setup/setup.sh
@@ -1,1 +1,8 @@
-echo 'Not implemented yet :('
+# Install apps used in the dotfiles
+./install_stack.sh
+
+# Setup symlinks for configuration files for those symlinks
+./setup_symlinks.sh
+
+# Setup theme symlink
+./theme_symlinks.sh
