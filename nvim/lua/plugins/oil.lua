a3657d9c4f19d381b480617bcc9c954399e6d9cc
Michal Pielka <mpielka726@gmail.com>
2025-12-01 20:00:23 +0100

    feat: add oil.lua

---
+++ nvim/lua/plugins/oil.lua
@@ -0,0 +1,14 @@
+return {
+  "stevearc/oil.nvim",
+  lazy = false,
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  opts = {
+    default_file_explorer = true,
+    skip_confirm_for_simple_edits = true,
+    view_options = {
+      sort = { { "name", "asc" } },
+    },
+  },
+
+	vim.keymap.set('n', '<leader>e', '<CMD>Oil --float<CR>', { desc = "Open Oil" } )
+}
