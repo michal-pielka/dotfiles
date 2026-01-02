3a5b6f15cc6c15e66cdd1cc9742c659c439be63e
Michal Pielka <mpielka726@gmail.com>
2026-01-02 23:07:51 +0100

    feat: add zcomet plugin manager with git and syntax highlighting plugins

--- zsh/zsh_custom/plugins.zsh
+++ zsh/zsh_custom/plugins.zsh
@@ -1,4 +1,12 @@
-# Oh My Zsh and plugins
-plugins=(zsh-syntax-highlighting git vi-mode)
+# ---- zcomet init ----
+if [[ ! -f ${ZDOTDIR:-${HOME}}/.zcomet/bin/zcomet.zsh ]]; then
+  git clone https://github.com/agkozak/zcomet.git ${ZDOTDIR:-${HOME}}/.zcomet/bin
+fi
 
-# source $ZSH/oh-my-zsh.sh
+source ${ZDOTDIR:-${HOME}}/.zcomet/bin/zcomet.zsh
+
+# ---- plugins ----
+zcomet load zsh-users/zsh-syntax-highlighting
+zcomet load ohmyzsh plugins/git
+
+zcomet compinit
