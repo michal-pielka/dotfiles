f35900700791e6f90352c1ccd984a14125ac6334
Michal Pielka <mpielka726@gmail.com>
2026-01-03 01:51:33 +0100

    feat: up/down arrows match typed command from history

--- zsh/zsh_custom/keybindings.zsh
+++ zsh/zsh_custom/keybindings.zsh
@@ -1,3 +1,10 @@
+# History search with prefix matching
+autoload -U history-search-end
+zle -N history-beginning-search-backward-end history-search-end
+zle -N history-beginning-search-forward-end history-search-end
+bindkey '^[[A' history-beginning-search-backward-end  # Up arrow
+bindkey '^[[B' history-beginning-search-forward-end    # Down arrow
+
 # Ctrl + Z will fg the job that's in the background.
 fancy-ctrl-z () {
 	if [[ $#BUFFER -eq 0 ]]; then
