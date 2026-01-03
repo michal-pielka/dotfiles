e375a31b3df1a17e623233e101bf5e1c42800f80
Michal Pielka <mpielka726@gmail.com>
2026-01-03 04:22:19 +0100

    docs(keybindings): better comments

--- zsh/zsh_custom/keybindings.zsh
+++ zsh/zsh_custom/keybindings.zsh
@@ -2,8 +2,8 @@
 autoload -U history-search-end
 zle -N history-beginning-search-backward-end history-search-end
 zle -N history-beginning-search-forward-end history-search-end
-bindkey '^[[A' history-beginning-search-backward-end  # Up arrow
-bindkey '^[[B' history-beginning-search-forward-end    # Down arrow
+bindkey '^[[A' history-beginning-search-backward-end 	# Up arrow
+bindkey '^[[B' history-beginning-search-forward-end		# Down arrow
 
 # Ctrl + Z will fg the job that's in the background.
 fancy-ctrl-z () {
