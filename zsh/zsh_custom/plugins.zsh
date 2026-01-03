eec58bc1df896ad549bf90cde9cfba754265c8bd
Michal Pielka <mpielka726@gmail.com>
2026-01-03 04:23:04 +0100

    docs(plugins): better comments

--- zsh/zsh_custom/plugins.zsh
+++ zsh/zsh_custom/plugins.zsh
@@ -1,11 +1,11 @@
-# ---- zcomet init ----
+# Zcomet init
 if [[ ! -f ${ZDOTDIR:-${HOME}}/.zcomet/bin/zcomet.zsh ]]; then
   git clone https://github.com/agkozak/zcomet.git ${ZDOTDIR:-${HOME}}/.zcomet/bin
 fi
 
 source ${ZDOTDIR:-${HOME}}/.zcomet/bin/zcomet.zsh
 
-# ---- plugins ----
+# Loading/installing plugins
 zcomet load zsh-users/zsh-syntax-highlighting
 zcomet load ohmyzsh plugins/git
 
