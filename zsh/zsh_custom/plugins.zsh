25ff37ef0f31027ae554a37a78e6507bf4c85453
Michal Pielka <mpielka726@gmail.com>
2026-01-29 08:06:10 +0100

    feat: migrate to 'better' zsh vim mode

--- zsh/zsh_custom/plugins.zsh
+++ zsh/zsh_custom/plugins.zsh
@@ -8,6 +8,6 @@ source ${ZDOTDIR:-${HOME}}/.zcomet/bin/zcomet.zsh
 # Loading/installing plugins
 zcomet load zsh-users/zsh-syntax-highlighting
 zcomet load ohmyzsh plugins/git
-zcomet load softmoth/zsh-vim-mode
+zcomet load jeffreytse/zsh-vi-mode
 
 zcomet compinit
