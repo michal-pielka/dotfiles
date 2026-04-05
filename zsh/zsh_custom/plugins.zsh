948f16350b6621d89f80ba76c344dbe3e22ffcb5
Michal Pielka <mpielka726@gmail.com>
2026-04-05 19:53:03 +0200

    feat: use system's clipboard for yank/paste in zsh-vi-mode

--- zsh/zsh_custom/plugins.zsh
+++ zsh/zsh_custom/plugins.zsh
@@ -8,6 +8,18 @@ source ${ZDOTDIR:-${HOME}}/.zcomet/bin/zcomet.zsh
 # Loading/installing plugins
 zcomet load zsh-users/zsh-syntax-highlighting
 zcomet load ohmyzsh plugins/git
+
+# System clipboard for zsh-vi-mode (y/p syncs with Wayland clipboard)
+ZVM_SYSTEM_CLIPBOARD_ENABLED=true
+ZVM_CLIPBOARD_COPY_CMD='wl-copy'
+ZVM_CLIPBOARD_PASTE_CMD='wl-paste -n'
+
+# Rebind p/P to paste from system clipboard instead of internal kill ring
+zvm_after_lazy_keybindings_commands+=(
+  'zvm_bindkey vicmd "p" zvm_paste_clipboard_after'
+  'zvm_bindkey vicmd "P" zvm_paste_clipboard_before'
+)
+
 zcomet load jeffreytse/zsh-vi-mode
 
 zcomet compinit
